dc54f90f101fc041d3d506fd29601be2743a79f0
MrBaNkS <banksmi09@gmail.com>
2014-04-21 22:03:18 -0500

    Stop building VoicePlus

--- configs/common.mk
+++ configs/common.mk
@@ -24,8 +24,7 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/banks/overlays/common
 PRODUCT_PACKAGES += \
     Launcher3 \
     Stk \
-    Torch \
-    VoicePlus
+    Torch
 
 # Cyanogenmod superuser support
 SUPERUSER_EMBEDDED := true
